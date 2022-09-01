e9dcb1d15226ea5aac53283f0b41648fe75e5a6b
Elhazzime Salaheddin <officielsalah1970@gmail.com>
2022-09-01 16:06:04 +0100

    update code

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-var dns = require("dns");
+const dns = require("dns/promises");
+var unicode = require("unidecode");
+const tldEnum = require("tld-enum");
 
-function checkAvailable(url) {
-  //uses the core modules to run an IPv4 resolver that returns 'err' on error
-  dns.resolve4(url, function (err, addr) {
-    if (addr) {
-      console.log("domain name registered");
-    }
-    if (err) console.log(url + " is possibly available : " + err);
+// const list = tldEnum.list;
+const list = ["com", "org", "net", "int", "edu", "gov", "mil"];
+
+const prefixes = [
+  "google",
+  "yotta",
+  "zetta",
+  "exa",
+  "peta",
+  "tera",
+  "giga",
+  "mega",
+  "kilo",
+  "hecto",
+  "deka",
+  "deci",
+  "centi",
+  "milli",
+  "micro",
+  "nano",
+  "pico",
+  "femto",
+  "atto",
+  "zepto",
+  "yocto",
+];
+
+let registered = [];
+
+const asynccheckAvailable = async (url) => {
+  await dns.resolve4(url).then((res) => {
+    registered.push(url);
   });
-}
-// calls the function of a given url
-checkAvailable("google.com");
+};
+
+(async function () {
+  for (let pre of prefixes) {
+    for (let li of list) {
+      try {
+        await asynccheckAvailable(pre + "." + unicode(li));
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+
+  console.log(registered);
+})();
