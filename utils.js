0035b393118fcb6608530b053d242c5da449f328
Elhazzime Salaheddin <officielsalah1970@gmail.com>
2022-09-02 22:00:16 +0100

    alter between sync and async

---
+++ utils.js
@@ -0,0 +1,31 @@
+function generateDomainName(X, arr) {
+  let ml = [];
+  let result = [];
+  ml = arr;
+  for (let i = 0; i < ml.length; i++) {
+    result.push(ml[i]);
+  }
+
+  let count = ml.length;
+
+  for (let z = 0; z < X - 1; z++) {
+    let tmp = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let k = 0; k < ml.length; k++) {
+        if (arr[i] != ml[k]) {
+          tmp.push(ml[k] + arr[i]);
+          count += 1;
+        }
+      }
+    }
+
+    for (let i = 0; i < tmp.length; i++) {
+      result.push(tmp[i]);
+    }
+
+    ml = tmp;
+  }
+  return result;
+}
+module.exports = { generateDomainName };
