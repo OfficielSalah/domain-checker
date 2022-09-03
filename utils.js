1ead08982330c6c25a0b8a2413acfaafe384627f
Elhazzime Salaheddin <officielsalah1970@gmail.com>
2022-09-03 08:54:46 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -28,4 +28,28 @@ function generateDomainName(X, arr) {
   }
   return result;
 }
-module.exports = { generateDomainName };
+function* generateDomainName2(X, arr) {
+  let ml = [];
+  ml = arr;
+  for (let i = 0; i < ml.length; i++) {
+    yield ml[i];
+  }
+
+  let count = ml.length;
+
+  for (let z = 0; z < X - 1; z++) {
+    let tmp = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let k = 0; k < ml.length; k++) {
+        if (arr[i] != ml[k]) {
+          tmp.push(ml[k] + arr[i]);
+          yield ml[k] + arr[i];
+          count += 1;
+        }
+      }
+    }
+    ml = tmp;
+  }
+}
+module.exports = { generateDomainName, generateDomainName2 };
